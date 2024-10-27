e4b8831b734b114103dfddc5ddc27847d91b88cb
Unai Sarasola <unaisarasola@hotmail.com>
2024-10-27 13:17:15 +0100

    Country example

--- Requirements.md
+++ Requirements.md
@@ -3,6 +3,8 @@ Requirements:
 - php-curl
 - php-xml
 - php-gd
+- php-mysql
 
 Pending:
 - Steps to deploy in prod/docker
+- Check if is needed to include the mariadb driver or just use mysql driver for basic operation
--- basic/config/db.php
+++ basic/config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=172.17.0.2;dbname=mam',
+    'username' => 'mam',
+    'password' => 'complex-password',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
