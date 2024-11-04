6879b1d5052551f82aa20e7437f89cf1a12512e3
Unai Sarasola <unaisarasola@hotmail.com>
2024-11-04 18:52:50 +0100

    Remove index from view

--- basic/views/country/view.php
+++ basic/views/country/view.php
@@ -29,7 +29,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'id',
             'name',
             'iso2_code',
         ],
