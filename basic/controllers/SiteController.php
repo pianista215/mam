05eb36899d9bd6a04b8fca3fa4836fc6c82bf5db
Unai Sarasola <unaisarasola@hotmail.com>
2024-11-15 21:03:07 +0100

    Basic toggle admin

--- basic/controllers/SiteController.php
+++ basic/controllers/SiteController.php
@@ -117,6 +117,23 @@ class SiteController extends Controller
         ]);
     }
 
+    // TODO: REMOVE WHEN NO LONGER IS NEEDED TO SET ADMIN BY THAT
+    // index.php?r=site%2Ftoggle-admin-role
+    public function actionToggleAdminRole() {
+        if (!Yii::$app->user->isGuest) {
+            $auth = Yii::$app->authManager;
+            $actualRoles = $auth->getRolesByUser(Yii::$app->user->id);
+            if(isset($actualRoles['admin'])) {
+                $adminRole = $actualRoles['admin'];
+                $auth->revoke($adminRole, Yii::$app->user->id);
+            } else {
+                $adminRole = $auth->getRole('admin');
+                $auth->assign($adminRole, Yii::$app->user->id);
+            }
+        }
+        return $this->redirect(['index']);
+    }
+
     // TODO: REMOVE WHEN ROLES ARE FINISHED AND ALL IS SET IN SQL FOR DB
     // index.php?r=site%2Fgenerate-roles
     public function actionGenerateRoles()
