600f969a6abb134c36392c621286f70bca51fa01
Unai Sarasola <unaisarasola@hotmail.com>
2024-12-08 15:58:42 +0100

    Route test

--- basic/tests/unit/DbTestCase.php
+++ basic/tests/unit/DbTestCase.php
@@ -16,6 +16,7 @@ abstract class DbTestCase extends \Codeception\Test\Unit
     {
         $db = Yii::$app->db;
         Yii::$app->db->createCommand()->delete('pilot')->execute();
+        Yii::$app->db->createCommand()->delete('route')->execute();
         Yii::$app->db->createCommand()->delete('airport')->execute();
         Yii::$app->db->createCommand()->delete('country')->execute();
     }
---
+++ basic/tests/unit/models/RouteTest.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace tests\unit\models;
+
+use app\models\Airport;
+use app\models\Country;
+use app\models\Route;
+use tests\unit\DbTestCase;
+use Yii;
+
+class RouteTest extends DbTestCase
+{
+
+    protected function _before()
+    {
+        parent::_before();
+
+        $country = new Country(['id' => 1, 'name' => 'Spain', 'iso2_code' => 'ES']);
+        $country->save(false);
+
+        $airport1 = new Airport([
+            'icao_code' => 'LEMD',
+            'name' => 'Madrid-Barajas',
+            'latitude' => 40.471926,
+            'longitude' => -3.56264,
+            'city' => 'Madrid',
+            'country_id' => 1,
+        ]);
+        $airport1->save(false);
+
+        $airport2 = new Airport([
+            'icao_code' => 'LEBL',
+            'name' => 'Barcelona-El Prat',
+            'latitude' => 41.297445,
+            'longitude' => 2.0833,
+            'city' => 'Barcelona',
+            'country_id' => 1,
+        ]);
+        $airport2->save(false);
+    }
+
+    public function testCreateValidRoute()
+    {
+        $route = new Route([
+            'code' => 'MAD-BCN',
+            'departure' => 'LEMD',
+            'arrival' => 'LEBL',
+        ]);
+        $this->assertTrue($route->validate());
+        $this->assertTrue($route->save());
+        $this->assertNotEmpty($route->distance_nm);
+    }
+
+    public function testUniqueCode()
+    {
+        $route1 = new Route([
+            'code' => 'MAD-BCN',
+            'departure' => 'LEMD',
+            'arrival' => 'LEBL',
+        ]);
+        $route1->save(false);
+
+        $route2 = new Route([
+            'code' => 'MAD-BCN',
+            'departure' => 'LEMD',
+            'arrival' => 'LEBL',
+        ]);
+        $this->assertFalse($route2->validate());
+        $this->assertArrayHasKey('code', $route2->getErrors());
+    }
+
+    public function testUniqueDepartureArrivalPair()
+    {
+        $route1 = new Route([
+            'code' => 'MAD-BCN',
+            'departure' => 'LEMD',
+            'arrival' => 'LEBL',
+        ]);
+        $route1->save(false);
+
+        $route2 = new Route([
+            'code' => 'MAD-BCN-ALT',
+            'departure' => 'LEMD',
+            'arrival' => 'LEBL',
+        ]);
+        $this->assertFalse($route2->validate());
+        $this->assertArrayHasKey('departure', $route2->getErrors());
+    }
+
+    public function testInvalidDeparture()
+    {
+        $route = new Route([
+            'code' => 'INVALID-BCN',
+            'departure' => 'XXXX',
+            'arrival' => 'LEBL',
+        ]);
+        $this->assertFalse($route->validate());
+        $this->assertArrayHasKey('departure', $route->getErrors());
+    }
+
+    public function testInvalidArrival()
+    {
+        $route = new Route([
+            'code' => 'MAD-INVALID',
+            'departure' => 'LEMD',
+            'arrival' => 'YYYY',
+        ]);
+        $this->assertFalse($route->validate());
+        $this->assertArrayHasKey('arrival', $route->getErrors());
+    }
+
+    public function testMaxLengthCode()
+    {
+        $route = new Route([
+            'code' => str_repeat('A', 11),
+            'departure' => 'LEMD',
+            'arrival' => 'LEBL',
+        ]);
+        $this->assertFalse($route->validate());
+        $this->assertArrayHasKey('code', $route->getErrors());
+    }
+
+    public function testDistanceCalculation()
+    {
+        $route = new Route([
+            'code' => 'MAD-BCN',
+            'departure' => 'LEMD',
+            'arrival' => 'LEBL',
+        ]);
+        $this->assertTrue($route->save(false));
+        $this->assertEqualsWithDelta(261, $route->distance_nm, 1); // Approximate distance with a margin
+    }
+
+}
