6a037eb664252aebc19f2d5b4de41d06d8961398
Unai Sarasola <unaisarasola@hotmail.com>
2024-12-11 21:29:11 +0100

    Migration

--- migration/get_aircraft_types.py
+++ migration/get_aircraft_types.py
@@ -16,6 +16,8 @@ cnxmam = mysql.connector.connect(user=args.user, password=args.password, host=ar
 
 cursorvam = cnxvam.cursor()
 cursormam = cnxmam.cursor()
+
+#Common versions (ICAO 4 chars)
 query = ("select plane_icao, plane_description, maximum_range, pax, cargo_capacity  from fleettypes where length(plane_icao)=4 AND maximum_range REGEXP '^[0-9]+$' ORDER BY plane_icao")
 
 cursorvam.execute(query)
@@ -25,6 +27,20 @@ imported_aircraft_types = 0
 for aircraft_type in cursorvam:
 	cursormam.execute("INSERT INTO aircraft_type(icao_type_code, name, max_nm_range) VALUES (%s,%s,%s)", (aircraft_type[0], aircraft_type[1], aircraft_type[2]))
 	cursormam.execute("INSERT INTO aircraft_configuration(aircraft_type_id, name, pax_capacity, cargo_capacity) select id,'Standard',%s,%s FROM aircraft_type WHERE icao_type_code=%s", (aircraft_type[3], aircraft_type[4], aircraft_type[0]))
+	imported_aircraft_types  = imported_aircraft_types + 1
+
+#Migrate cargo versions (ICAO 5 chars ending in F)
+query = ("select plane_icao, plane_description, maximum_range, pax, cargo_capacity  from fleettypes where length(plane_icao)=5 AND maximum_range REGEXP '^[0-9]+$' AND RIGHT(plane_icao,1)='F' ORDER BY plane_icao")
+
+cursorvam.execute(query)
+
+for aircraft_type in cursorvam:
+	real_icao = aircraft_type[0][0:4]
+	cursormam.execute("select count(*) from aircraft_type where icao_type_code=%s", (real_icao,))
+	if cursormam.fetchone()[0] == 0:
+		cursormam.execute("INSERT INTO aircraft_type(icao_type_code, name, max_nm_range) VALUES (%s,%s,%s)", (real_icao, aircraft_type[1], aircraft_type[2]))
+
+	cursormam.execute("INSERT INTO aircraft_configuration(aircraft_type_id, name, pax_capacity, cargo_capacity) select id,'Cargo',%s,%s FROM aircraft_type WHERE icao_type_code=%s", (aircraft_type[3], aircraft_type[4], real_icao))
 	imported_aircraft_types  = imported_aircraft_types + 1
 
 print("%d aircraft types imported from %s into %s" % (imported_aircraft_types, args.fromdb, args.destinationdb))
