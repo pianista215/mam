1050ec637e66c5160eb4c7707d67f518fb52ec9b
Unai Sarasola <unaisarasola@hotmail.com>
2024-12-22 19:18:31 +0100

    Route update

--- basic/views/route/update.php
+++ basic/views/route/update.php
@@ -5,7 +5,7 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var app\models\Route $model */
 
-$this->title = 'Update Route: ' . $model->id;
+$this->title = 'Update Route: ' . $model->code;
 $this->params['breadcrumbs'][] = ['label' => 'Routes', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
