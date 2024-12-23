ac1bf5a32b4ee2dba62d8ab4c71c1fcb6e56a2cf
Unai Sarasola <unaisarasola@hotmail.com>
2024-12-23 20:01:52 +0100

    Delete

---
+++ basic/tests/functional/aircraftType/AircraftTypeDeleteCest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace tests\functional\aircraftType;
+
+use tests\fixtures\AircraftTypeFixture;
+use tests\fixtures\AuthAssignmentFixture;
+use Yii;
+
+class AircraftTypeDeleteCest
+{
+
+    // TODO: Create acceptance tests to delete (lack of JS Support, POST not available in codeception API)
+
+    public function _fixtures(){
+        return [
+            'authAssignment' => AuthAssignmentFixture::class,
+            'aircraftType' => AircraftTypeFixture::class,
+        ];
+    }
+
+    public function deleteAircraftTypeAsAdmin(\FunctionalTester $I)
+    {
+        $I->amLoggedInAs(2);
+        $I->amOnRoute('aircraft-type/view', [ 'id' => '1' ]);
+
+        $I->see('Delete');
+    }
+
+    public function deleteOnlyPostAsAdmin(\FunctionalTester $I)
+    {
+        $I->amLoggedInAs(2);
+        $I->amOnRoute('aircraft-type/delete', [ 'id' => '1' ]);
+        $I->seeResponseCodeIs(405);
+        $count = \app\models\AircraftType::find()->count();
+        $I->assertEquals(3, $count);
+    }
+
+    public function deleteOnlyPostAsUser(\FunctionalTester $I)
+    {
+        $I->amLoggedInAs(1);
+        $I->amOnRoute('aircraft-type/delete', [ 'id' => '1' ]);
+        $I->seeResponseCodeIs(405);
+        $count = \app\models\AircraftType::find()->count();
+        $I->assertEquals(3, $count);
+    }
+
+    public function deleteOnlyPostAsVisitor(\FunctionalTester $I)
+    {
+        $I->amOnRoute('aircraft-type/delete', [ 'id' => '1' ]);
+        $I->seeResponseCodeIs(405);
+        $count = \app\models\AircraftType::find()->count();
+        $I->assertEquals(3, $count);
+    }
+
+}
