f3cc933138b7943fc4997c79762169c21839fa67
Unai Sarasola <unaisarasola@hotmail.com>
2024-12-28 17:38:39 +0100

    Can see all possibilities

--- basic/tests/functional/submitFlightPlan/SubmittedFlightPlanIndexViewCest.php
+++ basic/tests/functional/submitFlightPlan/SubmittedFlightPlanIndexViewCest.php
@@ -54,61 +54,42 @@ class SubmittedFlightPlanIndexViewCest
 
     public function openAircraftIndexAsVfrValidator(\FunctionalTester $I)
     {
-       $I->amLoggedInAs(4);
-       $this->canSeeTheAircraftIndex($I);
-       $I->see('Departure');
-       $I->see('Arrival');
+        $I->amLoggedInAs(4);
+        $this->canSeeTheAircraftIndex($I);
+        $I->see('Departure');
+        $I->see('Arrival');
 
-       $I->see('Vfr School');
-       $I->dontSee('Ifr School');
-       $I->dontSee('Other Ifr School');
-       $I->dontSee('Ifr Validator');
-       $this->canSeeOnlyViewButton($I);
+        $I->see('Vfr School');
+        $I->dontSee('Ifr School');
+        $I->dontSee('Other Ifr School');
+        $I->dontSee('Ifr Validator');
+        $this->canSeeOnlyViewButton($I);
     }
 
     public function openAircraftIndexAsIfrValidator(\FunctionalTester $I)
     {
-       $I->amLoggedInAs(5);
-       $this->canSeeTheAircraftIndex($I);
-       $I->see('Departure');
-       $I->see('Arrival');
-       $I->dontSee('Vfr School');
-       $I->see('Ifr School');
-       $I->see('Other Ifr School');
-       $I->see('Ifr Validator');
-       $this->canSeeOnlyViewButton($I);
+        $I->amLoggedInAs(5);
+        $this->canSeeTheAircraftIndex($I);
+        $I->see('Departure');
+        $I->see('Arrival');
+        $I->dontSee('Vfr School');
+        $I->see('Ifr School');
+        $I->see('Other Ifr School');
+        $I->see('Ifr Validator');
+        $this->canSeeOnlyViewButton($I);
     }
 
     public function openAircraftIndexAsAdmin(\FunctionalTester $I)
     {
         $I->amLoggedInAs(2);
-
-        $I->amOnRoute('submitted-flight-plan/index');
-
-        $I->see('Submitted Flight Plans');
-        $I->see('Showing 1-3 of 3 items.');
-
-        $I->see('Boeing 737-800 (Standard)');
-        $I->see('Boeing Name Std');
-        $I->see('EC-AAA');
-        $I->see('LEMD');
-
-        $I->see('Boeing 737-800 (Cargo)');
-        $I->see('Boeing Name Cargo');
-        $I->see('EC-BBB');
-        $I->see('LEBL');
-
-        $I->see('Cessna 172 (Standard)');
-        $I->see('C172 Std');
-        $I->see('EC-UUU');
-        $I->see('LEBL');
-
-        $this->checkAircraftIndexCommon($I);
-
-        $I->see('Create Aircraft', 'a');
-        $I->seeElement('a', ['title' => 'View']);
-        $I->seeElement('a', ['title' => 'Update']);
-        $I->seeElement('a', ['title' => 'Delete']);
+        $this->canSeeTheAircraftIndex($I);
+        $I->see('Departure');
+        $I->see('Arrival');
+        $I->see('Vfr School');
+        $I->see('Ifr School');
+        $I->see('Other Ifr School');
+        $I->see('Ifr Validator');
+        $this->canSeeOnlyViewButton($I);
     }
 
 }
