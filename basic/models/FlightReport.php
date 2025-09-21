472902a2e98b34466d0bd219e8a1997f19d8e4de
Unai Sarasola <unaisarasola@hotmail.com>
2025-09-21 17:35:35 +0200

    Move validator comments

--- basic/models/FlightReport.php
+++ basic/models/FlightReport.php
@@ -16,7 +16,6 @@ use Yii;
  * @property int|null $total_fuel_burn_kg
  * @property int|null $distance_nm
  * @property string|null $pilot_comments
- * @property string|null $validator_comments
  * @property int|null $initial_fuel_on_board
  * @property int|null $zero_fuel_weight
  * @property int|null $crash
@@ -45,8 +44,8 @@ class FlightReport extends \yii\db\ActiveRecord
             [['flight_id', 'flight_time_minutes', 'block_time_minutes', 'total_fuel_burn_kg', 'distance_nm', 'initial_fuel_on_board', 'zero_fuel_weight', 'crash'], 'integer'],
             [['start_time', 'end_time'], 'safe'],
             [['start_time', 'end_time'], 'datetime', 'format' => 'php:Y-m-d H:i:s'],
-            [['pilot_comments', 'validator_comments', 'sim_aircraft_name'], 'trim'],
-            [['pilot_comments', 'validator_comments'], 'string', 'max' => 400],
+            [['pilot_comments', 'sim_aircraft_name'], 'trim'],
+            [['pilot_comments'], 'string', 'max' => 400],
             [['sim_aircraft_name'], 'string', 'max' => 50],
             [['flight_id'], 'unique'],
             [['flight_id'], 'exist', 'skipOnError' => true, 'targetClass' => Flight::class, 'targetAttribute' => ['flight_id' => 'id']],
@@ -68,7 +67,6 @@ class FlightReport extends \yii\db\ActiveRecord
             'total_fuel_burn_kg' => 'Total Fuel Burn Kg',
             'distance_nm' => 'Distance Nm',
             'pilot_comments' => 'Pilot Comments',
-            'validator_comments' => 'Validator Comments',
             'initial_fuel_on_board' => 'Initial Fuel On Board',
             'zero_fuel_weight' => 'Zero Fuel Weight',
             'crash' => 'Crash',
