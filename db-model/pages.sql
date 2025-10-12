fbd16a8027657d34a30d92b6e2ef058c38ec46ac
Unai Sarasola <unaisarasola@hotmail.com>
2025-10-12 16:08:32 +0200

    Rules

--- db-model/pages.sql
+++ db-model/pages.sql
@@ -15,7 +15,7 @@ Meet the team behind our virtual airline. Dedicated professionals working every 
 
 ---
 
-### John M. Carter
+### John M. C.
 <div style="display: flex; align-items: flex-start; gap: 10px; margin-bottom: 20px;">
     <img src="/uploads/staff/john.jpeg" alt="John Carter" width="150">
     <div>
@@ -31,7 +31,7 @@ Meet the team behind our virtual airline. Dedicated professionals working every 
 
 ---
 
-### Emily R. Stone
+### Emily R. S.
 <div style="display: flex; align-items: flex-start; gap: 10px; margin-bottom: 20px;">
     <img src="/uploads/staff/emily.jpeg" alt="Emily Stone" width="150">
     <div>
@@ -47,7 +47,7 @@ Meet the team behind our virtual airline. Dedicated professionals working every 
 
 ---
 
-### Michael D. Harris
+### Michael D. H.
 <div style="display: flex; align-items: flex-start; gap: 10px; margin-bottom: 20px;">
     <img src="/uploads/staff/michael.jpeg" alt="Michael Harris" width="150">
     <div>
@@ -63,7 +63,7 @@ Meet the team behind our virtual airline. Dedicated professionals working every 
 
 ---
 
-### Laura S. Bennett
+### Laura S. B.
 <div style="display: flex; align-items: flex-start; gap: 10px; margin-bottom: 20px;">
     <img src="/uploads/staff/laura.jpeg" alt="Laura Bennett" width="150">
     <div>
@@ -76,6 +76,24 @@ Meet the team behind our virtual airline. Dedicated professionals working every 
         <strong>VATSIM VID:</strong> XXXXXX
     </div>
 </div>
+',
+    NOW(),
+    NOW()
+);
+
+INSERT INTO page_content (page_id, language, title, content_md, created_at, updated_at)
+VALUES (
+    (SELECT id FROM page WHERE code = 'rules'),
+    'en',
+    'Rules',
+'
+# General Info
+## Access information
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+# Registration
+## Entry and registration
+Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 ',
     NOW(),
     NOW()
