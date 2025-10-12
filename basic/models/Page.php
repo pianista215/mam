fe975ab13f45bbb345f19b4b17d48a46b17a9bd8
Unai Sarasola <unaisarasola@hotmail.com>
2025-10-12 11:33:10 +0200

    Pages

---
+++ basic/models/Page.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "page".
+ *
+ * @property int $id
+ * @property string $code
+ * @property string $created_at
+ * @property string $updated_at
+ *
+ * @property PageContent[] $pageContents
+ */
+class Page extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'page';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['code'], 'required'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['code'], 'string', 'max' => 50],
+            [['code'], 'unique'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'code' => 'Code',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * Gets query for [[PageContents]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPageContents()
+    {
+        return $this->hasMany(PageContent::class, ['page_id' => 'id']);
+    }
+}
---
+++ basic/models/PageContent.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "page_content".
+ *
+ * @property int $id
+ * @property int $page_id
+ * @property string $language
+ * @property string $content_md
+ * @property string $created_at
+ * @property string $updated_at
+ *
+ * @property Page $page
+ */
+class PageContent extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'page_content';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['page_id', 'language', 'content_md'], 'required'],
+            [['page_id'], 'integer'],
+            [['content_md'], 'string'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['language'], 'string', 'max' => 2],
+            [['page_id', 'language'], 'unique', 'targetAttribute' => ['page_id', 'language']],
+            [['page_id'], 'exist', 'skipOnError' => true, 'targetClass' => Page::class, 'targetAttribute' => ['page_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'page_id' => 'Page ID',
+            'language' => 'Language',
+            'content_md' => 'Content Md',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+        ];
+    }
+
+    /**
+     * Gets query for [[Page]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getPage()
+    {
+        return $this->hasOne(Page::class, ['id' => 'page_id']);
+    }
+}
