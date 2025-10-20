a1986e18a5b73a99388d709f14aa9aeba1a29ffc
Unai Sarasola <unaisarasola@hotmail.com>
2025-10-20 17:19:31 +0200

    Page

--- basic/controllers/PageController.php
+++ basic/controllers/PageController.php
@@ -3,9 +3,11 @@
 namespace app\controllers;
 
 use app\models\Page;
+use Yii;
 use yii\data\ActiveDataProvider;
 use yii\helpers\Markdown;
 use yii\web\Controller;
+use yii\web\ForbiddenHttpException;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 
@@ -20,6 +22,10 @@ class PageController extends Controller
         $page = Page::find()->where(['code' => $code])->one();
         if (!$page) {
             throw new NotFoundHttpException("Page not found: $code");
+        }
+
+        if(!$page->public && Yii::$app->user->isGuest) {
+            throw new ForbiddenHttpException('You are not allowed to access this page');
         }
 
         $content = $page->getPageContents()->where(['language' => $lang])->one();
