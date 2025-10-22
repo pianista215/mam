cb1f7578b84a16e401d6d46e03967e4172fe1cf7
Unai Sarasola <unaisarasola@hotmail.com>
2025-10-22 21:29:05 +0200

    GeoUtils

--- basic/models/Route.php
+++ basic/models/Route.php
@@ -3,6 +3,7 @@
 namespace app\models;
 
 use app\helpers\CustomRules;
+use app\helpers\GeoUtils;
 use Yii;
 
 /**
@@ -90,20 +91,6 @@ class Route extends \yii\db\ActiveRecord
         return $this->hasMany(SubmittedFlightplan::class, ['route_id' => 'id']);
     }
 
-    /**
-     * Calculate the distance between twi points in NM (https://gist.github.com/teachmeter/3014803)
-     * TODO: Better as static function outside here????
-     */
-    protected function distanceBetween($lat1, $lon1, $lat2, $lon2)
-    {
-        $theta = $lon1 - $lon2;
-        $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
-        $dist = acos($dist);
-        $dist = rad2deg($dist);
-        $miles = $dist * 60 * 1.1515;
-        return $miles * 0.8684;
-    }
-
     public function beforeSave($insert)
     {
         if (parent::beforeSave($insert)) {
@@ -112,7 +99,7 @@ class Route extends \yii\db\ActiveRecord
             }
             $dep = $this->departure0;
             $arr = $this->arrival0;
-            $this->distance_nm = $this->distanceBetween($dep->latitude, $dep->longitude, $arr->latitude, $arr->longitude);
+            $this->distance_nm = GeoUtils::haversine($dep->latitude, $dep->longitude, $arr->latitude, $arr->longitude, 'nm');
             return true;
         }
         return false;
