f808248bf5a6e01ae21528b89b93778819f3e983
Unai Sarasola <unaisarasola@hotmail.com>
2025-11-08 10:06:46 +0100

    Create rank

---
+++ basic/tests/functional/rank/RankIndexViewCest.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace tests\functional\route;
+
+use tests\fixtures\AuthAssignmentFixture;
+use tests\fixtures\RouteFixture;
+use Yii;
+
+class RouteIndexViewCest
+{
+    public function _fixtures(){
+        return [
+            'authAssignment' => AuthAssignmentFixture::class,
+            'route' => RouteFixture::class,
+        ];
+    }
+
+    private function checkRouteIndexCommon(\FunctionalTester $I){
+        $I->amOnRoute('route/index');
+
+        $I->see('Routes');
+        $I->see('Showing 1-3 of 3 items.');
+        $I->see('LEBL');
+        $I->see('LEVC');
+        $I->see('GCLP');
+        $I->see('R001');
+        $I->see('R002');
+        $I->see('R003');
+    }
+
+    public function openRouteIndexAsAdmin(\FunctionalTester $I)
+    {
+        $I->amLoggedInAs(2);
+
+        $this->checkRouteIndexCommon($I);
+
+        $I->see('Create Route', 'a');
+        $I->seeElement('a', ['title' => 'View']);
+        $I->seeElement('a', ['title' => 'Update']);
+        $I->seeElement('a', ['title' => 'Delete']);
+    }
+
+    public function openRouteIndexAsUser(\FunctionalTester $I)
+    {
+        $I->amLoggedInAs(1);
+
+        $this->checkRouteIndexCommon($I);
+
+        $I->dontSee('Create Route', 'a');
+        $I->seeElement('a', ['title' => 'View']);
+        $I->dontSeeElement('a', ['title' => 'Update']);
+        $I->dontSeeElement('a', ['title' => 'Delete']);
+    }
+
+    public function openRouteIndexAsVisitor(\FunctionalTester $I)
+    {
+        $this->checkRouteIndexCommon($I);
+
+        $I->dontSee('Create Route', 'a');
+        $I->seeElement('a', ['title' => 'View']);
+        $I->dontSeeElement('a', ['title' => 'Update']);
+        $I->dontSeeElement('a', ['title' => 'Delete']);
+    }
+
+    private function checkAircraftConfigViewCommon(\FunctionalTester $I) {
+        $I->amOnRoute('route/view', [ 'id' => '1' ]);
+
+        $I->see('LEBL');
+        $I->see('LEVC');
+        $I->see('R001');
+        $I->see('165');
+    }
+
+    public function openRouteViewAsAdmin(\FunctionalTester $I)
+    {
+        $I->amLoggedInAs(2);
+
+        $this->checkAircraftConfigViewCommon($I);
+
+        $I->see('Update', 'a');
+        $I->see('Delete', 'a');
+    }
+
+    public function openRouteViewAsUser(\FunctionalTester $I)
+    {
+        $I->amLoggedInAs(1);
+
+        $this->checkAircraftConfigViewCommon($I);
+
+        $I->dontSee('Update', 'a');
+        $I->dontSee('Delete', 'a');
+    }
+
+    public function openRouteViewAsVisitor(\FunctionalTester $I)
+    {
+        $I->amOnRoute('route/view', [ 'id' => '1' ]);
+        // Check redirect
+        $I->seeCurrentUrlMatches('~login~');
+        $I->see('Login');
+    }
+
+
+}
