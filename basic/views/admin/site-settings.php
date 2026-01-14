61da938e088fbe2447da000b24286b3c9dc1ba9c
Unai Sarasola <unaisarasola@hotmail.com>
2026-01-14 11:19:12 +0100

    Settings

--- basic/views/admin/site-settings.php
+++ basic/views/admin/site-settings.php
@@ -5,29 +5,42 @@ use yii\helpers\Html;
 $this->title = Yii::t('app', 'Site Settings');
 ?>
 
-<h1><?= $this->title ?></h1>
+<h1><?= Html::encode($this->title) ?></h1>
 
 <?php $form = ActiveForm::begin(); ?>
 
-<h3><?=Yii::t('app', 'Registration Settings')?></h3>
+<?php if ($model->hasErrors()): ?>
+    <div class="alert alert-danger">
+        <strong><?= Yii::t('app', 'Please fix the following errors:') ?></strong>
+        <ul>
+            <?php foreach ($model->getErrors() as $attrErrors): ?>
+                <?php foreach ($attrErrors as $error): ?>
+                    <li><?= Html::encode($error) ?></li>
+                <?php endforeach; ?>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+<?php endif; ?>
+
+<h3><?= Yii::t('app', 'Registration Settings') ?></h3>
 <?= $form->field($model, 'registration_start')->input('date') ?>
 <?= $form->field($model, 'registration_end')->input('date') ?>
 <?= $form->field($model, 'registration_start_location')->textInput(['maxlength' => true]) ?>
 
-<h3><?=Yii::t('app', 'Storage')?></h3>
+<h3><?= Yii::t('app', 'Storage') ?></h3>
 <?= $form->field($model, 'chunks_storage_path') ?>
 <?= $form->field($model, 'images_storage_path') ?>
 
-<h3><?=Yii::t('app', 'Security')?></h3>
+<h3><?= Yii::t('app', 'Security') ?></h3>
 <?= $form->field($model, 'token_life_h')->input('number') ?>
 <?= $form->field($model, 'charter_ratio')->input('number', ['step' => '0.01']) ?>
 
-<h3><?=Yii::t('app', 'Global')?></h3>
+<h3><?= Yii::t('app', 'Global') ?></h3>
 <?= $form->field($model, 'airline_name') ?>
 <?= $form->field($model, 'no_reply_mail') ?>
 <?= $form->field($model, 'support_mail') ?>
 
-<h3><?=Yii::t('app', 'Footer')?></h3>
+<h3><?= Yii::t('app', 'Footer') ?></h3>
 <?= $form->field($model, 'x_url') ?>
 <?= $form->field($model, 'instagram_url') ?>
 <?= $form->field($model, 'facebook_url') ?>
