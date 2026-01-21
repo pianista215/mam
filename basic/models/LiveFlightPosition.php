ef2397e1c4396cb58fced1bc85fe8c16a0e40df4
Unai Sarasola <unaisarasola@hotmail.com>
2026-01-21 17:10:04 +0100

    Model and ddl

---
+++ basic/models/LiveFlightPosition.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "live_flight_position".
+ *
+ * @property int $submitted_flight_plan_id
+ * @property float $latitude
+ * @property float $longitude
+ * @property int $altitude
+ * @property int $heading
+ * @property int $ground_speed
+ * @property string $updated_at
+ *
+ * @property SubmittedFlightPlan $submittedFlightPlan
+ */
+class LiveFlightPosition extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'live_flight_position';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['submitted_flight_plan_id', 'latitude', 'longitude', 'altitude', 'heading', 'ground_speed'], 'required'],
+            [['submitted_flight_plan_id', 'altitude', 'heading', 'ground_speed'], 'integer'],
+            [['latitude', 'longitude'], 'number'],
+            [['updated_at'], 'safe'],
+            [['submitted_flight_plan_id'], 'unique'],
+            [['submitted_flight_plan_id'], 'exist', 'skipOnError' => true, 'targetClass' => SubmittedFlightPlan::class, 'targetAttribute' => ['submitted_flight_plan_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'submitted_flight_plan_id' => Yii::t('app', 'Submitted Flight Plan'),
+            'latitude' => Yii::t('app', 'Latitude'),
+            'longitude' => Yii::t('app', 'Longitude'),
+            'altitude' => Yii::t('app', 'Altitude'),
+            'heading' => Yii::t('app', 'Heading'),
+            'ground_speed' => Yii::t('app', 'Ground Speed'),
+            'updated_at' => Yii::t('app', 'Updated At'),
+        ];
+    }
+
+    /**
+     * Gets query for [[SubmittedFlightPlan]].
+     *
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSubmittedFlightPlan()
+    {
+        return $this->hasOne(SubmittedFlightPlan::class, ['id' => 'submitted_flight_plan_id']);
+    }
+}
