0c41c84a33054b1210770228339ba7e5f870777c
Unai Sarasola <unaisarasola@hotmail.com>
2026-01-29 23:01:28 +0100

    Generate month names

--- basic/views/statistics/month.php
+++ basic/views/statistics/month.php
@@ -15,20 +15,12 @@ use app\models\StatisticRecordType;
 use yii\helpers\Html;
 use yii\helpers\Url;
 
-$monthNames = [
-    1 => Yii::t('app', 'January'),
-    2 => Yii::t('app', 'February'),
-    3 => Yii::t('app', 'March'),
-    4 => Yii::t('app', 'April'),
-    5 => Yii::t('app', 'May'),
-    6 => Yii::t('app', 'June'),
-    7 => Yii::t('app', 'July'),
-    8 => Yii::t('app', 'August'),
-    9 => Yii::t('app', 'September'),
-    10 => Yii::t('app', 'October'),
-    11 => Yii::t('app', 'November'),
-    12 => Yii::t('app', 'December'),
-];
+// Generate month names using ICU (automatically translated based on app language)
+$monthNames = [];
+for ($m = 1; $m <= 12; $m++) {
+    $date = new DateTimeImmutable("2024-{$m}-01");
+    $monthNames[$m] = ucfirst(Yii::$app->formatter->asDate($date, 'LLLL')); // Standalone month name
+}
 
 $this->title = Yii::t('app', 'Monthly Statistics') . ' - ' . $monthNames[$month] . ' ' . $year;
 $this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Statistics'), 'url' => ['month']];
